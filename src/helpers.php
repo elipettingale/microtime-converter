64b3f7a517a372e79baf449a1246d93e04fea3ee
Elliot Pettingale <elipettingale@gmail.com>
2018-09-05 12:15:03 +0100

    create a class to separate and store the data

--- src/helpers.php
+++ src/helpers.php
@@ -2,33 +2,8 @@
 
 if (!function_exists('convert_microtime'))
 {
-    function convert_microtime($microtime): string
+    function convert_microtime($microtime): \EliPett\MicrotimeConverter\Services\MicroTime
     {
-        $hours = (int)
-            ($minutes = (int)
-                ($seconds = (int)
-                    ($milliseconds = (int)
-                    ($microtime * 1000))
-                    / 1000)
-                / 60)
-            / 60;
-
-        $minutes = $minutes % 60;
-        $seconds = $seconds % 60;
-        $milliseconds = $milliseconds % 1000;
-
-        if ($hours !== 0) {
-            return "Elapsed Time: {$hours} hour(s) {$minutes} minute(s) {$seconds} second(s) {$milliseconds} millisecond(s)";
-        }
-
-        if ($minutes !== 0) {
-            return "Elapsed Time: {$minutes} minute(s) {$seconds} second(s) {$milliseconds} millisecond(s)";
-        }
-
-        if ($seconds !== 0) {
-            return "Elapsed Time: {$seconds} second(s) {$milliseconds} millisecond(s)";
-        }
-
-        return "Elapsed Time: {$milliseconds} millisecond(s)";
+        return new \EliPett\MicrotimeConverter\Services\MicroTime($microtime);
     }
 }
